528fcc4af0cc5ccb27addf6d433c0de06f3fab59
John Constant <johnconstantis@gmail.com>
2023-01-29 21:23:34 +0000

    Add basic view and template for menu app

--- burger_bar/urls.py
+++ burger_bar/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
     path('accounts/', include('allauth.urls')),
+    path('menu/', include('menu.urls')),
     path('', include('home.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- menu/views.py
+++ menu/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.views import generic, View
+
+from .models import Menu_Item, Menu_Category, Special_Offer
+
 
 # Create your views here.
+class MenuList(generic.ListView):
+    """
+    A class view for getting a specific category
+    """
+    def get(self, request, *args, **kwargs):
+        items = Menu_Item.objects.all()
+        categories = Menu_Category.objects.all()
+
+        context = {
+            'items': items,
+            'categories': categories
+        }
+
+        return render(request, 'menu/menu.html', context)
