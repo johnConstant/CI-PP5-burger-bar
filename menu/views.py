39578239b2bc6dec991f8415ba6a66e77382813d
John Constant <johnconstantis@gmail.com>
2023-02-05 18:19:12 +0000

    Add menu information and sorting to menu page

--- menu/views.py
+++ menu/views.py
@@ -36,9 +36,9 @@ def MenuList(request):
             menu_items = menu_items.order_by(sortkey)
 
         if 'category' in request.GET:
-            categories = request.GET['category'].split(',')
-            menu_items = menu_items.filter(category__name__in=categories)
-            categories = Menu_Category.objects.filter(name__in=categories)
+            category = request.GET['category']
+            menu_items = menu_items.filter(category__name=category)
+            category = Menu_Category.objects.filter(name=category)
 
         if 'search' in request.GET:
             query = request.GET['search']
@@ -53,10 +53,13 @@ def MenuList(request):
                 )
             menu_items = menu_items.filter(queries)
 
+    current_sorting = f'{sort}_{direction}'
+
     context = {
         "menu_items": menu_items,
         "search": query,
-        "current_category": categories,
+        "current_category": category,
+        "current_sorting": current_sorting
     }
 
     return render(request, 'menu/menu.html', context)
