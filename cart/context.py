b3de2f645f24fac74a81d4b622d67f441bc57a6e
John Constant <johnconstantis@gmail.com>
2023-02-07 20:53:04 +0000

    Update context.py in cart and add order table to cart.html template

--- cart/context.py
+++ cart/context.py
@@ -1,12 +1,25 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from menu.models import Menu_Item
 
 
 def cart_contents(request):
 
     cart_items = []
     total = 0
-    product_count = 0
+    menu_item_count = 0
+    cart = request.session.get('cart', {})
+
+    for item_id, quantity in cart.items():
+        menu_item = get_object_or_404(Menu_Item, pk=item_id)
+        total += quantity * menu_item.price
+        menu_item_count += quantity
+        cart_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'menu_item': menu_item,
+        })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = settings.STANDARD_DELIVERY_AMOUNT
@@ -20,7 +33,7 @@ def cart_contents(request):
     context = {
         'cart_items': cart_items,
         'total': total,
-        'product_count': product_count,
+        'menu_item_count': menu_item_count,
         'delivery': delivery,
         'free_delivery_delta': free_delivery_delta,
         'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
