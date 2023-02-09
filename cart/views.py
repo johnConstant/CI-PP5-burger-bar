dff5ca9f20787a319ea48af4825e49ec2a8ee71a
John Constant <johnconstantis@gmail.com>
2023-02-09 21:01:29 +0000

    Add adjust quantity functionality to cart.html

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 
 # Create your views here.
 
@@ -22,5 +22,19 @@ def add_to_cart(request, item_id):
         cart[item_id] = quantity
 
     request.session['cart'] = cart
-    print(request.session['cart'])
     return redirect(redirect_url)
+
+
+def adjust_cart(request, item_id):
+    """Adjust the quantity of the specified product to the specified amount"""
+    quantity = int(request.POST.get('quantity'))
+    cart = request.session.get('cart', {})
+    print(quantity)
+    if quantity > 0:
+        cart[item_id] = quantity
+    else:
+        cart.pop(item_id)
+
+    request.session['cart'] = cart
+    print(request.session['cart'])
+    return redirect(reverse('view_cart'))
