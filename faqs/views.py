b5987389f0a017476b68377d4786b5c6850f48df
John Constant <johnconstantis@gmail.com>
2023-02-11 16:05:08 +0000

    Add basic faq view and template

--- faqs/views.py
+++ faqs/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from .models import FAQ
+
+
+class FAQList(generic.ListView):
+    """
+    A class view for getting all categories
+    """
+    model = FAQ
+    queryset = FAQ.objects.all()
+    template_name = 'faqs/faqs.html'
