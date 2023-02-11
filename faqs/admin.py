98332a87cc749f9476514628f417c9c0d1cb9137
John Constant <johnconstantis@gmail.com>
2023-02-11 16:21:09 +0000

    Update FAQs admin and template

--- faqs/admin.py
+++ faqs/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import FAQ
+from django_summernote.admin import SummernoteModelAdmin
 
 # Register your models here.
+
+
+@admin.register(FAQ)
+class FAQAdmin(SummernoteModelAdmin):
+
+    list_display = ('question', 'answer')
+    search_fields = ['question', 'answer']
+    summernote_fields = ('question', 'answers')
