143b9f244fcc1fa338c521b76241080e51f4655b
John Constant <johnconstantis@gmail.com>
2023-02-14 22:07:44 +0000

    Add locations admin

--- location/admin.py
+++ location/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Location, Hours
 
-# Register your models here.
+
+class HoursAdminInline(admin.TabularInline):
+    model = Hours
+
+
+class LocationAdmin(admin.ModelAdmin):
+    inlines = (HoursAdminInline,)
+    prepopulated_fields = ({'slug': ('name',)})
+    fields = ('name', 'slug',
+              'email', 'phone_number', 'country',
+              'postcode', 'town_or_city', 'street_address1',
+              'street_address2', 'county', 'latitude',
+              'longitude',)
+    list_display = ('name', 'street_address1', 'phone_number')
+
+
+admin.site.register(Order, OrderAdmin)
