9c44a24c9cee12a418cd3b4a84565586abade9b4
John Constant <johnconstantis@gmail.com>
2023-02-23 20:12:17 +0000

    Update locations model and template

--- location/models.py
+++ location/models.py
@@ -37,7 +37,9 @@ class Location(models.Model):
     postcode = models.CharField(max_length=20, null=True, blank=True)
     latitude = models.DecimalField(max_digits=8, decimal_places=6)
     longitude = models.DecimalField(max_digits=8, decimal_places=6)
-    opening_hours = models.ManyToManyField(Hours, related_name='opening_hours', blank=True)
+    opening_hours = models.ManyToManyField(
+        Hours, related_name='opening_hours', blank=True
+        )
 
     def __str__(self):
         return self.name
