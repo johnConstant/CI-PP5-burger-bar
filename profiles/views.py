c754331f747a0cd3725e8d098a8e717ca05617a6
John Constant <johnconstantis@gmail.com>
2023-03-04 13:56:36 +0000

    Add profile and orders to view and template

--- profiles/views.py
+++ profiles/views.py
@@ -1,10 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import UserProfile
 
 
 def profile(request):
     """ Display the user's profile. """
+    profile = get_object_or_404(UserProfile, user=request.user)
+    orders = profile.orders.all()
 
     template = 'profiles/profile.html'
-    context = {}
+    context = {
+        'profile': profile,
+        'orders': orders
+    }
 
     return render(request, template, context)
