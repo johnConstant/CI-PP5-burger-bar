98bfacf4be92986010c0e7d63abbb2dc4c28cb36
John Constant <johnconstantis@gmail.com>
2023-03-04 18:07:39 +0000

    Add confirmation email to checkout process in webhook handler

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -1,5 +1,9 @@
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.conf import settings
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
+
 from .models import Order, OrderLineItem
 from location.models import Location
 from menu.models import Menu_Item
@@ -15,6 +19,23 @@ class StripeWH_Handler:
 
     def __init__(self, request):
         self.request = request
+
+    def _send_confirmation_email(self, order):
+        """Send the user a confirmation email"""
+        customer_email = order.email
+        subject = render_to_string(
+            'checkout/confirmation_emails/confirmation_email_subject.txt',
+            {'order': order})
+        body = render_to_string(
+            'checkout/confirmation_emails/confirmation_email_body.txt',
+            {'order': order, 'contact_email': settings.DEFAULT_FROM_EMAIL})
+
+        send_mail(
+            subject,
+            body,
+            settings.DEFAULT_FROM_EMAIL,
+            [customer_email]
+        )  
 
     def handle_event(self, event):
         """
@@ -89,6 +110,7 @@ class StripeWH_Handler:
                 attempt += 1
                 time.sleep(1)
         if order_exists:
+            self._send_confirmation_email(order)
             return HttpResponse(
                 content=f'Webhook received: {event["type"]} | SUCCESS:\
                      Verified order already in database',
@@ -128,6 +150,7 @@ class StripeWH_Handler:
                 return HttpResponse(
                     content=f'Webhook received: {event["type"]} | ERROR: {e}',
                     status=500)
+        self._send_confirmation_email(order)
         return HttpResponse(
             content=f'Webhook received: {event["type"]} | SUCCESS: Created\
                  order in webhook',
