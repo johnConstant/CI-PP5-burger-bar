c1abc013f5e5787c2dfe2e0e32e75a13d88e2d37
John Constant <johnconstantis@gmail.com>
2023-03-06 20:50:29 +0000

    Add 'edit location' view, form, template and url

--- location/urls.py
+++ location/urls.py
@@ -8,6 +8,9 @@ urlpatterns = [
          'add/', views.LocationAdd.as_view(), name='add_location'
     ),
     path(
+         'edit/<slug:slug>/', views.LocationUpdate.as_view(), name='edit_location'
+    ),
+    path(
         '<slug:slug>/', views.LocationDetail.as_view(), name='location_detail'
     ),
 ]
