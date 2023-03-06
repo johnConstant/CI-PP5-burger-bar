1378fb4cffe5c6c485d54ea1ac2caf0cfcceaf6c
John Constant <johnconstantis@gmail.com>
2023-03-06 21:48:15 +0000

    Fix issue with name of contact url

--- contact/urls.py
+++ contact/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.Contact.as_view(), name='locations'),
+    path('', views.Contact.as_view(), name='contact'),
 ]
