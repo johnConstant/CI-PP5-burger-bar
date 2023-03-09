67399ae88947a8b81d9ec4fe4e4c9055fd4926e8
John Constant <johnconstantis@gmail.com>
2023-03-09 13:21:28 +0000

    Add Privacy Policy page and link in footer

--- home/views.py
+++ home/views.py
@@ -12,3 +12,9 @@ def index(request):
     }
 
     return render(request, 'home/index.html', context)
+
+
+def privacy_policy(request):
+    """ A view to return the privacy policy page """
+
+    return render(request, 'home/privacy_policy.html')
