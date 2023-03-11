c155d2a00a5381f7b9964cdd6c1f237714317a69
John Constant <johnconstantis@gmail.com>
2023-03-11 16:35:12 +0000

    Add Profile and Contact admin classes

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Contact
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(Contact)
+class ContactAdmin(SummernoteModelAdmin):
+
+    list_display = ('name', 'email', 'message')
+    list_filter = ('email_subscription',)
+    search_fields = ['name', 'email', 'message']
+    summernote_fields = ('message',)
--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import UserProfile
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(UserProfile)
+class UserProfileAdmin(SummernoteModelAdmin):
+
+    list_display = ('user', )
+    list_filter = ('default_location', )
+    search_fields = ['name', 'email', ]
